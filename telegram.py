c8b9f5241fc971878a91f269627f76697eeb2d9f
Behroz666 <BehrozBesharatnia@gmail.com>
2025-07-15 17:34:18 +0330

    add phone

--- telegram.py
+++ telegram.py
@@ -6,12 +6,13 @@ import os
 api_id = int(os.environ["TG_API_ID"])
 api_hash = os.environ["TG_API_HASH"]
 bot_token = os.environ["TG_BOT_TOKEN"]
+phone = os.environ["phone"]
 
 async def fetch_recent_posts(channel_username: str, days: int = 1) -> list:
 
     client = TelegramClient("session", api_id, api_hash)
 
-    await client.start()
+    await client.start(phone)
 
     entity = await client.get_entity(channel_username)
     time_threshold = datetime.utcnow() - timedelta(days=days)
